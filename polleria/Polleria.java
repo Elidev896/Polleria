93eb070b426ce324b5aba127dd1f3069750093d6
Eliseo <ledesmagomezeliseo@gmail.com>
2024-02-23 14:31:47 -0300

    first commit

---
+++ polleria/Polleria.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package polleria;
+
+import vistas.Vprincipal;
+
+/**
+ *
+ * @author elise
+ */
+public class Polleria {
+
+  
+    public static void main(String[] args) {
+        Vprincipal vp = new Vprincipal();
+        vp.setVisible(true);
+    }
+    
+}
